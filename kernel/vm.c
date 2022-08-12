f6eef517fa212aa34911705980ceaf50d0354a90
Maxim Rudik <m.rudik@omp.ru>
2022-08-12 13:52:15 +0300

    Implement pte printout

--- kernel/vm.c
+++ kernel/vm.c
@@ -440,3 +440,34 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+void
+vmprint(pagetable_t pagetable)
+{
+  print_pagetable(pagetable, 0);
+}
+
+void
+print_pagetable(pagetable_t pagetable, int level)
+{
+  if(level > 2)
+    panic("print_pagetable level");
+  else if(level == 0)
+    printf("page table %p\n", pagetable);
+
+  for(int i = 0; i < 512; i++){
+    pte_t pte = pagetable[i];
+
+    if(pte & PTE_V) {
+      printf("..");
+      for(int i = 0; i < level; i++)
+        printf(" ..");
+      printf("%d: pte %p pa %p\n", i, pte, PTE2PA(pte));
+
+      if((pte & (PTE_R|PTE_W|PTE_X)) == 0){
+        uint64 child = PTE2PA(pte);
+        print_pagetable((pagetable_t)child, level + 1);
+      }
+    }
+  }
+}
